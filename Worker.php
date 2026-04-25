89fc36abc2ace1c6a706020ffac6a61a3e03fed9
ep1088 <ep1088@qq.com>
2026-04-25 13:33:43 +0800

    fix $workerId, Implicit conversion from float

--- Worker.php
+++ Worker.php
@@ -262,7 +262,7 @@ class EventSelect
             $timer_id             = $scheduler_data['data'];
             $next_run_time        = -$scheduler_data['priority'];
             $time_now             = \microtime(true);
-            $this->_selectTimeout = ($next_run_time - $time_now) * 1000000;
+            $this->_selectTimeout = (int)(($next_run_time - $time_now) * 1000000);
             if ($this->_selectTimeout <= 0) {
                 $this->_scheduler->extract();
 
@@ -997,6 +997,13 @@ class Worker
      * @var bool
      */
     protected static $_outputDecorated = null;
+
+    /**
+     * Worker object's hash id(unique identifier).
+     *
+     * @var ?string
+     */
+    protected $workerId = null;
 
     /**
      * Run all worker instances.
